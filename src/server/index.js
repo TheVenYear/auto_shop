eb0472ca13caa97dc500b72e7616f3022e54e3d9
TheVenYear <TheVenYear@gmail.com>
2021-04-12 23:26:22 +0300

    dsadsa

--- src/server/index.js
+++ src/server/index.js
@@ -11,19 +11,12 @@ app.use(cors());
 app.use(express.json());
 app.use('/api', routes);
 
-<<<<<<< HEAD
-app.use(express.static(path.resolve(__dirname, 'build')));
-app.use('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', '..', 'build', 'index.html'));
-});
-=======
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.resolve(__dirname, 'build')));
   app.use('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '..', '..', 'build', 'index.html'));
   });
 }
->>>>>>> 8cc170e99d6f1591162ee94585fa81454143080c
 
 app.listen(PORT, () => {
   console.log('Server is running on port ' + PORT);
