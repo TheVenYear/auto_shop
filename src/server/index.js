8cc170e99d6f1591162ee94585fa81454143080c
Ivan Korchagin <47949509+TheVenYear@users.noreply.github.com>
2021-04-12 22:43:51 +0300

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -14,7 +14,7 @@ app.use('/api', routes);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.resolve(__dirname, 'build')));
   app.use('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '..', '..', 'build', 'index.html'));
   });
 }
 
