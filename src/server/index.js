22e7598ca72c492b9d1444ec9799058be3e7030d
TheVenYear <TheVenYear@gmail.com>
2021-04-12 21:08:22 +0300

    build

--- src/server/index.js
+++ src/server/index.js
@@ -5,11 +5,19 @@ const app = express();
 const cors = require('cors');
 
 const routes = require('./routes');
+const path = require('path');
 
 app.use(cors());
 app.use(express.json());
 app.use('/api', routes);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.resolve(__dirname, 'build')));
+  app.use('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  });
+}
+
 app.listen(PORT, () => {
-   console.log('Server is running on port ' + PORT);
+  console.log('Server is running on port ' + PORT);
 });
